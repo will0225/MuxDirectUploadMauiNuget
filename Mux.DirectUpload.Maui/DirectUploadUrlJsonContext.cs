6a63576e27c03a86f72904c0c6fdae65e139f5a8
will0225 <kjm729@tutamail.com>
2026-04-12 19:52:59 -0600

    latest update for package

--- Mux.DirectUpload.Maui/DirectUploadUrlJsonContext.cs
+++ Mux.DirectUpload.Maui/DirectUploadUrlJsonContext.cs
@@ -2,8 +2,12 @@ using System.Text.Json.Serialization;
 
 namespace Mux.DirectUpload.Maui;
 
+/// <summary>
+/// Auth backends typically return camelCase JSON: <c>{"uploadUrl":"..."}</c>.
+/// </summary>
 internal sealed record DirectUploadUrlResponse(string UploadUrl);
 
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
 [JsonSerializable(typeof(DirectUploadUrlResponse))]
 internal sealed partial class DirectUploadUrlJsonContext : JsonSerializerContext { }
 
